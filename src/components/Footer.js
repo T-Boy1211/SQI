01eeffba134764b7881fcfa7a980563667519f0f
T-Boy1211 <ayanwolavictor1211@gmil.com>
2025-09-16 21:27:24 +0100

    footer updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,78 +2,101 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="flex flex-col-5 justify-between">
-      <div>
-        <h3 className="text-blue-400 text-2xl font-bold">Hello, we are SQI College of ICT</h3>
-        <h4 className="mt-6">We provide and lead ohers in quality ICT education</h4>
-        <h4 className="mt-15">Send us a mail</h4>
-        <p className="text-blue-500">
-          <a href="mailto">enquiry@edu.sqi.ng</a>
-        </p>
+    <footer className="flex flex-col pt-16">
+      <div className="px-30 flex flex-5 justify-around space-x-20">
+        <div>
+          <h3 className="text-blue-400 text-2xl font-bold w-50">
+            Hello, we are SQI College of ICT
+          </h3>
+          <h4 className="mt-6">
+            We provide and lead ohers in quality ICT education
+          </h4>
+          <h4 className="mt-15 text-stone-600">Send us a mail</h4>
+          <p className="text-blue-500">
+            <a href="mailto">enquiry@edu.sqi.ng</a>
+          </p>
+        </div>
+        <div className="py-3 space-y-5">
+          <h6 className="mt-3 text-lg w-50 font-bold">Quick Links</h6>
+          <p className="mt-10 text-stone-600">Application Portal</p>
+          <p className="text-stone-600">Student Portal</p>
+          <p className="text-stone-600">Professional Courses</p>
+          <p className="text-stone-600">NID Courses</p>
+          <p className="text-stone-600">Campus Info</p>
+          <p className="text-stone-600">Accomodation</p>
+          <p className="text-stone-600">SQI Scholarship</p>
+          <p className="text-stone-600">Donate</p>
+        </div>
+        <div className="py-3 space-y-5">
+          <h6 className="mt-3 text-lg font-bold w-50">Ogbomoso</h6>
+          <p className="text-stone-600">
+            Old Ilorin Road, Opposite Yoaco Filling Station, Yoaco, Ogbomoso.
+          </p>
+          <p className="text-stone-600">
+            <strong>0906 281 9991, 0906 281 9993</strong>
+          </p>
+          <p>
+            <a href="" className="text-blue-500">Locate on the map</a>
+          </p>
+        </div>
+        <div className="space-y-5">
+          <h6 className="mt-3 text-lg font-bold w-50">Ibadan</h6>
+          <p className="text-stone-600">
+            First Floor, H25 Heritage Mall, Opposite Central Bank of Nigeria,
+            Dugbe, Ibadan.
+          </p>
+          <p className="text-stone-600">
+            <strong>0906 281 9994</strong>
+          </p>
+          <p>
+            <a href="" className="text-blue-500">Locate on the map</a>
+          </p>
+          <p className="text-stone-600">
+            Christianah Oyinade Ajoke House, beside First Bank, Arisekola
+            Central Mosque, Opposite Jaiz bank, Idi Ape, Iwo road, Ibadan.
+          </p>
+          <p className="text-stone-600">
+            <strong>0906 281 9995</strong>
+          </p>
+          <p>
+            <a href="" className="text-blue-500">Locate on the map</a>
+          </p>
+          <p className="text-stone-600">
+            Haier Thermocool Building, opposite SAO filling station, Challenge,
+            Ibadan, Oyo State.
+          </p>
+          <p className="text-stone-600">
+            <strong>0906 281 9992</strong>
+          </p>
+          <p>
+            <a href="" className="text-blue-500">Locate on the map</a>
+          </p>
+        </div>
+        <div className="space-y-5">
+          <h6 className="mt-3 text-lg font-bold w-50">Abeokuta</h6>
+          <p className="text-stone-600">First floor, OPIC Tower building, Okeilewo, Abeokuta.</p>
+          <p className="text-stone-600">
+            <strong>0906 281 9996</strong>
+          </p>
+          <p>
+            <a href="" className="text-blue-500">Locate on the map</a>
+          </p>
+          <h6 className="text-stone-600">Osogbo</h6>
+          <p>Opposite Jaiz bank, Ogo-Oluwa, Osogbo</p>
+          <p className="text-stone-600">
+            <strong>0906 281 9997</strong>
+          </p>
+          <p>
+            <a href="" className="text-blue-500">Locate on the map</a>
+          </p>
+        </div>
       </div>
-      <div className="py-3">
-        <h6 className="mt-3 text-lg">Quick Links</h6>
-        <p className="mt-10">Application Portal</p>
-        <p>Student Portal</p>
-        <p>Professional Courses</p>
-        <p>NID Courses</p>
-        <p>Campus Info</p>
-        <p>Accomodation</p>
-        <p>SQI Scholarship</p>
-        <p>Donate</p>
-      </div>
-      <div className="py-3">
-        <h6 className="mt-3 text-lg">Ogbomoso</h6>
-        <p>Old Ilorin Road, Opposite Yoaco Filling Station, Yoaco, Ogbomoso.</p>
-        <p>
-          <strong>0906 281 9991, 0906 281 9993</strong>
-        </p>
-        <p>
-          <a href="">Locate on the map</a>
-        </p>
-      </div>
-      <div>
-        <h6>Ibadan</h6>
-        <p>
-          First Floor, H25 Heritage Mall, Opposite Central Bank of Nigeria,
-          Dugbe, Ibadan.
-        </p>
-        <p>
-          <strong>0906 281 9994</strong>
-        </p>
-        <p>
-          <a href="">Locate on the map</a>
-        </p>
-        <p>Christianah Oyinade Ajoke House, beside First Bank, Arisekola Central Mosque, Opposite Jaiz bank, Idi Ape, Iwo road, Ibadan.</p>
-        <p>
-          <strong>0906 281 9995</strong>
-        </p>
-        <p>
-          <a href="">Locate on the map</a>
-        </p>
-        <p>Haier Thermocool Building, opposite SAO filling station, Challenge, Ibadan, Oyo State.</p>
-        <p>
-          <strong>0906 281 9992</strong>
-        </p>
-        <p>
-          <a href="">Locate on the map</a>
-        </p>
-      </div>
-      <div>
-        <h6>Abeokuta</h6>
-        <p>First floor, OPIC Tower building, Okeilewo, Abeokuta.</p>
-        <p>
-          <strong>0906 281 9996</strong>
-        </p>
-        <p>
-          <a href="">Locate on the map</a>
-        </p>
-        <h6>Osogbo</h6>
-        <p>Opposite Jaiz bank, Ogo-Oluwa, Osogbo</p>
-        <p>
-          <strong>0906 281 9997</strong>
-        </p>
-        
+      <div className="bg-[#040036] mt-10">
+        <div>
+          <div className="px-40 pt-5">
+            <div>Copyright © {new Date().getFullYear()} | SQI ICT Consultants. All Right Reserved</div>
+          </div>
+        </div>
       </div>
     </footer>
   );
